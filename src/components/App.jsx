fb38cc41adcae9e518190c24217f4f625c3372f7
Tlangelani Mboweni <tlangelani@tinmandigital.co.za>
2021-01-20 14:33:08 +0200

    setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../sass/app.scss';
+
+const App = () => {
+    return (
+        <div className="cp-app">
+            App
+        </div>
+    )
+}
+
+export default App;
